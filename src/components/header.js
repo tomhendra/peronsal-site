19ce0791c6fe6e5d27963cdd7eac0063c97964f3
Thomas Hendra <tom.hendra@outlook.com>
2019-07-24 23:35:55 +0200

    emotion css styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,34 @@
-import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
+import { css } from '@emotion/core';
+
+import NavLink from './navlink';
 
 const Header = ({ siteTitle }) => (
   <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
+    css={css`
+      background-color: #eee;
+      border-bottom: 1px solid #ddd;
+      display: flex;
+      justify-content: space-between;
+      padding: 0.5rem calc((100vw - 550px - 0.5rem) / 2);
+    `}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+    <NavLink to="/" fontWeight="bold">
+      {siteTitle}
+    </NavLink>
+    <nav
+      css={css`
+        margin-top: 0;
+      `}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+      <NavLink to="/" activeClassName="current-page">
+        Home
+      </NavLink>
+      <NavLink to="/about/" activeClassName="current-page">
+        About
+      </NavLink>
+    </nav>
   </header>
 );
 
