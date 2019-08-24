3d7b993b2bcf01fecb63d12a170ccb0b205fd0bc
Thomas Hendra <tom.hendra@outlook.com>
2019-08-24 18:05:37 +0200

    Changed footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import useSiteMetadata from '../../hooks/use-site-metadata';
 import footerStyles from './footer-styles';
 
 const Footer = () => {
-  const { title } = useSiteMetadata();
   return (
     <footer css={footerStyles}>
-      <p>{title}</p>
+      <p>Web developer</p>
       <p>Social links</p>
     </footer>
   );
