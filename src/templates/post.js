ed4949a3d0220f130e618ea0f82cb8f60f07eaf8
Thomas Hendra <tom.hendra@outlook.com>
2019-09-05 10:49:11 +0200

    Post template - dynamic graphQL query based on ID - basic rendering for PostTemplate

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import { Layout } from '../components/layout';
 
-const postTemplate = () => {
-  return <Layout>This is the post template</Layout>;
-};
+export const query = graphql`
+  query BlogPostQuery($id: String) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      frontmatter {
+        title
+        author
+        date(formatString: "MMMM DD, YYYY")
+      }
+      html
+    }
+  }
+`;
+
+const postTemplate = ({ data: { markdownRemark } }) => (
+  <Layout>
+    <h1>{markdownRemark.frontmatter.title}</h1>
+    <p>
+      Posted by {markdownRemark.frontmatter.author} on{' '}
+      {markdownRemark.frontmatter.date}
+    </p>
+    <div dangerouslySetInnerHTML={{ __html: markdownRemark.html }}></div>
+  </Layout>
+);
 
 export default postTemplate;
