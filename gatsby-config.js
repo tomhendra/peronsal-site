a7bd8c88012a47001f535ba7d3b1fa445ad38577
Thomas Hendra <tom.hendra@outlook.com>
2019-09-05 12:30:37 +0200

    Post images - setup gatsby-transfomer-remark to facilitate images within posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,21 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
-    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-relative-images',
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 750,
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
