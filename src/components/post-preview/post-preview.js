8fea6bd8635688d4fa75569a0befe3c8b13a9266
Thomas Hendra <tom.hendra@outlook.com>
2019-09-06 10:25:11 +0200

    Contentful data source - graphQL query for usePosts hook revised to source Contentful data - PostPreview commented out images until this has been setup - changed excerpt to date in PostPreview until excerpts from Contentful has been configured

--- src/components/post-preview/post-preview.js
+++ src/components/post-preview/post-preview.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { css } from '@emotion/core';
-import { Link } from 'gatsby';
-import Image from 'gatsby-image';
+// import { css } from '@emotion/core';
+// import { Link } from 'gatsby';
+// import Image from 'gatsby-image';
 import { PostLink } from '../post-link';
 import { ReadLink } from '../read-link';
 import { postPreviewStyles } from './post-preview-styles';
 
 export const PostPreview = ({ post }) => (
   <article css={postPreviewStyles}>
-    <Link
+    {/* <Link
       to={post.slug}
       css={css`
         width: 100px;
       `}
     >
       <Image fluid={post.featureImage.sharp.fluid} alt={post.title} />
-    </Link>
+    </Link> */}
     <div>
       <h3>
         <PostLink to={post.slug}>{post.title}</PostLink>
       </h3>
-      <p>{post.excerpt}</p>
+      <p>{post.publishedDate}</p>
       <ReadLink to={post.slug}>read this post &rarr;</ReadLink>
     </div>
   </article>
--- src/hooks/use-posts.js
+++ src/hooks/use-posts.js
@@ -3,37 +3,62 @@ import { graphql, useStaticQuery } from 'gatsby';
 export function usePosts() {
   const data = useStaticQuery(graphql`
     query {
-      allMdx {
-        nodes {
-          id
-          frontmatter {
+      allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
+        edges {
+          node {
+            id
             title
-            date(formatString: "MMMM DD, YYYY")
-            author
-            featureImage {
-              sharp: childImageSharp {
-                fluid(maxWidth: 100, maxHeight: 100) {
-                  ...GatsbyImageSharpFluid_withWebp
-                }
-              }
-            }
-          }
-          excerpt
-          fields {
             slug
+            publishedDate(formatString: "Do MMMM, YYYY")
           }
         }
       }
     }
   `);
 
-  return data.allMdx.nodes.map(post => ({
-    id: post.id,
-    title: post.frontmatter.title,
-    date: post.frontmatter.date,
-    author: post.frontmatter.author,
-    featureImage: post.frontmatter.featureImage,
-    excerpt: post.excerpt,
-    slug: post.fields.slug,
+  return data.allContentfulBlogPost.edges.map(({ node }) => ({
+    id: node.id,
+    title: node.title,
+    slug: node.slug,
+    publishedDate: node.publishedDate,
   }));
 }
+
+// OLD MDX HOOK.......................................
+// export function usePosts() {
+//   const data = useStaticQuery(graphql`
+//     query {
+//       allMdx {
+//         nodes {
+//           id
+//           frontmatter {
+//             title
+//             date(formatString: "MMMM DD, YYYY")
+//             author
+//             featureImage {
+//               sharp: childImageSharp {
+//                 fluid(maxWidth: 100, maxHeight: 100) {
+//                   ...GatsbyImageSharpFluid_withWebp
+//                 }
+//               }
+//             }
+//           }
+//           excerpt
+//           fields {
+//             slug
+//           }
+//         }
+//       }
+//     }
+//   `);
+
+//   return data.allMdx.nodes.map(post => ({
+//     id: post.id,
+//     title: post.frontmatter.title,
+//     date: post.frontmatter.date,
+//     author: post.frontmatter.author,
+//     featureImage: post.frontmatter.featureImage,
+//     excerpt: post.excerpt,
+//     slug: post.fields.slug,
+//   }));
+// }
