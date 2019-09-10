02188063b6f505ee8d9a04deae455956e1635ea6
Thomas Hendra <tom.hendra@outlook.com>
2019-09-10 17:27:34 +0200

    Post template css - some inline css-in-js to limit content width - increased font size

--- src/templates/post.js
+++ src/templates/post.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import Img from 'gatsby-image';
+import { css } from '@emotion/core';
+import { size, fontsize } from '../assets/styles';
 import { Layout } from '../components/layout';
 
 export const query = graphql`
@@ -30,20 +32,36 @@ const postTemplate = ({ data: { contentfulBlogPost } }) => {
       'embedded-asset-block': node => {
         const alt = node.data.target.fields.title['en-US'];
         const url = node.data.target.fields.file['en-US'].url;
-        return <img src={url} alt={alt} />;
+        return (
+          <img
+            src={url}
+            alt={alt}
+            css={css`
+              max-width: ${size.massive3};
+            `}
+          />
+        );
       },
     },
   };
 
   return (
     <Layout>
-      <h1>{contentfulBlogPost.title}</h1>
-      <p>Posted on {contentfulBlogPost.publishedDate}</p>
-      <Img
-        fluid={contentfulBlogPost.featuredImage.fluid}
-        alt={contentfulBlogPost.title}
-      />
-      {documentToReactComponents(contentfulBlogPost.body.json, options)}
+      <div
+        css={css`
+          font-size: ${fontsize.medium1};
+          margin: 0 auto;
+          max-width: ${size.massive3};
+        `}
+      >
+        <h1>{contentfulBlogPost.title}</h1>
+        <p>Posted on {contentfulBlogPost.publishedDate}</p>
+        <Img
+          fluid={contentfulBlogPost.featuredImage.fluid}
+          alt={contentfulBlogPost.title}
+        />
+        {documentToReactComponents(contentfulBlogPost.body.json, options)}
+      </div>
     </Layout>
   );
 };
