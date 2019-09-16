d525e686e72521cf6d157dd4562a2282c2d3a3ab
Thomas Hendra <tom.hendra@outlook.com>
2019-09-16 22:31:10 +0200

    SkillGrid filter - introduced filter functionality - SkillGrid accepts array of skill titles to display with css grid

--- src/components/skill-grid/skill-grid.js
+++ src/components/skill-grid/skill-grid.js
@@ -1,16 +1,22 @@
+// In order to use this component it must be passed an array of
+// strings which must match the title specified in skills.yaml
+// exactly.
+
 import React from 'react';
 import { useSkills } from '../../hooks/use-skills';
 import { SkillItem } from '../skill-item';
 import { skillGridStyles } from './skill-grid-styles';
 
-export const SkillGrid = () => {
+export const SkillGrid = ({ skillsToBeDisplayed }) => {
   const skills = useSkills();
 
   return (
     <div css={skillGridStyles}>
-      {skills.map(skill => (
-        <SkillItem key={skill.id} skill={skill} />
-      ))}
+      {skills
+        .filter(skill => skillsToBeDisplayed.includes(skill.title))
+        .map(skill => (
+          <SkillItem key={skill.id} skill={skill} />
+        ))}
     </div>
   );
 };
--- src/pages/about.js
+++ src/pages/about.js
@@ -58,7 +58,42 @@ const AboutPage = () => (
         />
         <PageCounter pageNumber={`02`} totalPages={`03`} />
       </PageHeading>
-      <SkillGrid />
+      <SkillGrid
+        skillsToBeDisplayed={[
+          `JavaScript`,
+          `CSS`,
+          `HTML`,
+          `React`,
+          `Gatsby`,
+          `Webpack`,
+          `Emotion`,
+          `Sass`,
+          `CSS Modules`,
+        ]}
+      />
+    </Container>
+    <Container>
+      <PageHeading>
+        <PageText
+          mainHeading={`About me.`}
+          subHeading={`I get excited about the latest technologies.`}
+          paragraph={`I strive to build  web applications & websites that are accessible, performant, scalable and lightning fast.These are the technologies & tools of choice I use to get the job done.`}
+        />
+        <PageCounter pageNumber={`03`} totalPages={`03`} />
+      </PageHeading>
+      <SkillGrid
+        skillsToBeDisplayed={[
+          `Jest`,
+          `Testing Library`,
+          `Cypress`,
+          `GitHub`,
+          `NPM`,
+          `Yarn`,
+          `Firebase`,
+          `GraphQL`,
+          `Netlify`,
+        ]}
+      />
     </Container>
   </Layout>
 );
