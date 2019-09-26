c9f91214e3d26ab2e9b76aad756722c9b9812b61
Thomas Hendra <tom.hendra@outlook.com>
2019-09-26 18:18:14 +0200

    npm package update

--- src/components/contact-form/contact-form-styles.js
+++ src/components/contact-form/contact-form-styles.js
@@ -1,21 +1,26 @@
 import { css } from '@emotion/core';
-import { colors, sizes } from '../../assets/styles';
+import { colors, fontsizes, sizes } from '../../assets/styles';
 
-export const contactFormStyles = () => css`
-  align-items: center;
-  background: ${colors.neutral.dark2};
-  border: 1px solid ${colors.neutral.dark4};
-  border-radius: 6px;
-  display: flex;
-  flex-direction: column;
-  height: ${sizes.huge4};
-  margin: 0 auto;
-  padding: ${sizes.large2} ${sizes.medium3};
-  width: ${sizes.massive3};
+export const contactFormStyles = () =>
+  css({
+    alignItems: `center`,
+    background: colors.neutral.dark2,
+    border: `1px solid ${colors.neutral.dark4}`,
+    borderRadius: `6px`,
+    display: `flex`,
+    flexDirection: `column`,
+    height: sizes.huge4,
+    margin: `0 auto`,
+    padding: `${sizes.large2} ${sizes.medium3}`,
+    width: sizes.massive3,
 
-  > input {
-    background-color: ${colors.neutral.light5};
-    border-radius: 4px;
-    border: 1px solid ${colors.neutral.light2};
-  }
-`;
+    '& input': {
+      backgroundColor: colors.neutral.light5,
+      borderRadius: `4px`,
+      border: `1px solid ${colors.neutral.light2}`,
+      fontSize: fontsizes.base,
+      height: sizes.medium3,
+      padding: sizes.small2,
+      width: sizes.huge2,
+    },
+  });
--- src/components/contact-form/contact-form.js
+++ src/components/contact-form/contact-form.js
@@ -1,14 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { contactFormStyles } from './contact-form-styles';
 import { Button } from '../button';
 
-export const ContactForm = () => (
-  <form css={contactFormStyles}>
-    <input></input>
-    <input></input>
-    <input></input>
-    <Button type={`submit`} buttonStyle={`primary`} buttonSize={`small`}>
-      Send Message
-    </Button>
-  </form>
-);
+export const ContactForm = () => {
+  const initialState = {
+    fullName: '',
+    emailAddress: '',
+    phoneNumber: '',
+  };
+
+  const [state, setState] = useState(initialState);
+
+  const handleInputChange = event => {
+    console.log(event.target);
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    setState({
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert(
+      `Hello ${state.fullName}, your email address is ${state.emailAddress} & your phone number is ${state.phoneNumber}!`,
+    );
+  };
+
+  return (
+    <form
+      css={contactFormStyles}
+      method="post"
+      netlify-honeypot="bot-field"
+      data-netlify="true"
+      onSubmit={handleSubmit}
+    >
+      <input type="hidden" name="bot-field" />
+      <label>
+        Full name
+        <input
+          type="text"
+          name="fullName"
+          value={state.fullName}
+          onChange={handleInputChange}
+        />
+      </label>
+      <label>
+        Email address
+        <input
+          type="text"
+          name="emailAddress"
+          value={state.emailAddress}
+          onChange={handleInputChange}
+        />
+      </label>
+      <label>
+        Phone number
+        <input
+          type="text"
+          name="phoneNumber"
+          value={state.phoneNumber}
+          onChange={handleInputChange}
+        />
+      </label>
+      <Button type={`submit`} buttonStyle={`primary`} buttonSize={`small`}>
+        Send Message
+      </Button>
+    </form>
+  );
+};
