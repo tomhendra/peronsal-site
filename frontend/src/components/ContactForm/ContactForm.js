0547813a7d09c040a170a1c4e0e469482aea4cad
Thomas Hendra <tom.hendra@outlook.com>
2019-11-12 13:06:34 +0100

    feat(components): ContactForm - wired up form to use Formspree service - tested and all OK

--- frontend/src/components/ContactForm/ContactForm.js
+++ frontend/src/components/ContactForm/ContactForm.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import styled from '@emotion/styled';
@@ -8,8 +9,10 @@ import Card from '../Card';
 import TextInput from './components/TextInput';
 import Textarea from './components/Textarea';
 import Checkbox from './components/Checkbox';
+import ErrorMessage from './components/ErrorMessage';
 import ButtonGroup from '../ButtonGroup';
 import Button from '../Button';
+import Text from '../Text';
 
 import { buttons, sizes } from '../../assets/styles/constants';
 
@@ -37,115 +40,128 @@ const formStyles = ({ theme }) => ({
 
 const StyledForm = styled(Form)(formStyles);
 
-const encode = data => {
-  return Object.keys(data)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
-    .join('&');
-};
+const ContactForm = () => {
+  // Server State Handling
+  const [serverState, setServerState] = useState();
+  const handleServerResponse = (ok, msg) => {
+    setServerState({ ok, msg });
+  };
 
-const ContactForm = () => (
-  <Formik
-    initialValues={{
-      firstName: '',
-      lastName: '',
-      email: '',
-      message: '',
-      acceptedTerms: false, // for the checkbox
-    }}
-    validationSchema={Yup.object({
-      firstName: Yup.string()
-        .max(20, 'Must be 20 characters or less')
-        .required('Required'),
-      lastName: Yup.string()
-        .max(20, 'Must be 30 characters or less')
-        .required('Required'),
-      email: Yup.string()
-        .email('Invalid email address`')
-        .required('Required'),
-      message: Yup.string()
-        .max(500, 'Must be 500 characters or less')
-        .required('Required'),
-      acceptedTerms: Yup.boolean()
-        .required('Required')
-        .oneOf([true], 'Please accept the terms and conditions.'),
-    })}
-    onSubmit={(values, { setSubmitting, resetForm }) => {
-      fetch('/?no-cache=1', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: encode({
-          'form-name': 'contact',
-          ...values,
-        }),
+  const handleOnSubmit = async (values, actions) => {
+    await axios({
+      method: 'POST',
+      url: 'https://formspree.io/mayyjyqa',
+      data: values,
+    })
+      .then(() => {
+        actions.setSubmitting(false);
+        actions.resetForm();
+        handleServerResponse(
+          true,
+          'Thank you, your message has been submitted!',
+        );
       })
-        .then(() => {
-          alert('Success!');
-          setSubmitting(false);
-          resetForm();
-        })
-        .catch(error => {
-          alert('Error: Please Try Again!');
-          console.error(error);
-          setSubmitting(false);
-        });
-    }}
-  >
-    {/* destructure helper methods from props */}
-    {({ handleSubmit, isSubmitting, handleReset, dirty }) => (
-      <Card spacing={GOLF}>
-        <StyledForm
-          onSubmit={handleSubmit}
-          method="post"
-          data-netlify="true"
-          netlify-honeypot="bot-field"
-        >
-          <TextInput
-            label="First Name"
-            name="firstName"
-            type="text"
-            placeholder="Maggie"
-          />
-          <TextInput
-            label="Last Name"
-            name="lastName"
-            type="text"
-            placeholder="Simpson"
-          />
-          <TextInput
-            label="Email Address"
-            name="email"
-            type="email"
-            placeholder="maggie@fox.com"
-          />
-          <Textarea
-            label="Message"
-            name="message"
-            rows="8"
-            placeholder="Your message (maximum 500 characters)"
-          />
-          <Checkbox name="acceptedTerms">I accept the privacy policy</Checkbox>
-          <ButtonGroup>
-            <Button
-              buttonStyle={TERTIARY}
-              buttonSize={BRAVO}
-              disabled={isSubmitting || !dirty}
-              onClick={handleReset}
-            >
-              Reset
-            </Button>
-            <Button
-              buttonStyle={PRIMARY}
-              buttonSize={BRAVO}
-              type="submit"
-              disabled={isSubmitting || !dirty}
-            >
-              Send
-            </Button>
-          </ButtonGroup>
-        </StyledForm>
-      </Card>
-    )}
-  </Formik>
-);
+      .catch(error => {
+        actions.setSubmitting(false);
+        handleServerResponse(false, error.response.data.error);
+      });
+  };
+
+  return (
+    <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+        message: '',
+        acceptedTerms: false,
+      }}
+      // override onSubmit using custom function as advised by Formspree
+      // https://formspree.io/blog/react-forms-3/
+      onSubmit={handleOnSubmit}
+      validationSchema={Yup.object({
+        firstName: Yup.string()
+          .max(20, 'Must be 20 characters or less')
+          .required('Required'),
+        lastName: Yup.string()
+          .max(20, 'Must be 30 characters or less')
+          .required('Required'),
+        email: Yup.string()
+          .email('Invalid email address')
+          .required('Required'),
+        message: Yup.string()
+          .max(500, 'Must be 500 characters or less')
+          .required('Required'),
+        acceptedTerms: Yup.boolean()
+          .required('Required')
+          .oneOf([true], 'Please accept the terms and conditions.'),
+      })}
+    >
+      {/* destructure helper methods from props */}
+      {({ isSubmitting, handleReset }) => (
+        <Card spacing={GOLF}>
+          <StyledForm id="fs-frm" noValidate>
+            <input
+              type="hidden"
+              name="_subject"
+              value="New enquiry from tomhendra.dev"
+            />
+            <TextInput
+              label="First Name"
+              name="firstName"
+              type="text"
+              placeholder="Maggie"
+            />
+            <TextInput
+              label="Last Name"
+              name="lastName"
+              type="text"
+              placeholder="Simpson"
+            />
+            <TextInput
+              label="Email Address"
+              name="email"
+              type="email"
+              placeholder="maggie@fox.com"
+            />
+            <Textarea
+              label="Message"
+              name="message"
+              rows="8"
+              placeholder="Your message (maximum 500 characters)"
+            />
+            <Checkbox name="acceptedTerms">
+              I accept the privacy policy
+            </Checkbox>
+            <ButtonGroup>
+              <Button
+                buttonStyle={TERTIARY}
+                buttonSize={BRAVO}
+                disabled={isSubmitting}
+                onClick={handleReset}
+              >
+                Reset
+              </Button>
+              <Button
+                buttonStyle={PRIMARY}
+                buttonSize={BRAVO}
+                type="submit"
+                disabled={isSubmitting}
+              >
+                Send
+              </Button>
+            </ButtonGroup>
+            {serverState &&
+              (!serverState.ok ? (
+                <ErrorMessage>{serverState.msg}</ErrorMessage>
+              ) : (
+                <Text>{serverState.msg}</Text>
+              ))}
+          </StyledForm>
+        </Card>
+      )}
+    </Formik>
+  );
+};
 
 export default withTheme(ContactForm);
