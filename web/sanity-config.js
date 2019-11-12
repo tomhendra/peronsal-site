f7c8e9607b0d1f691bf01ca82fc74445a4b776ca
Thomas Hendra <tom.hendra@outlook.com>
2019-11-12 17:23:30 +0100

    Updated now config

--- web/sanity-config.js
+++ web/sanity-config.js
@@ -1,6 +1,6 @@
 // prettier-ignore
 module.exports = {
-  projectId: process.env.GATSBY_SANITY_PROJECT_ID || '<#< sanity.projectId >#>',
-  dataset: process.env.GATSBY_SANITY_DATASET || '<#< sanity.dataset >#>',
+  projectId: process.env.SANITY_PROJECT_ID || '<#< sanity.projectId >#>',
+  dataset: process.env.SANITY_DATASET || '<#< sanity.dataset >#>',
   token: process.env.SANITY_READ_TOKEN || '<#< sanity.readToken >#>',
 };
