d8b1e5526fcda53b130fdfc8780ff8379bcba9d7
Thomas Hendra <tom.hendra@outlook.com>
2019-11-26 20:06:57 +0100

    feat(pages): 404 tablet responsive layout - page done, copied from index more or less!

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -2,21 +2,39 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
-// import Container from '../components/Container';
-// import PageHeading from '../components/PageHeading';
-// import PageText from '../components/PageText';
+import Container from '../components/Container';
+import Graphic from '../components/Graphic';
+import Grid from '../components/Grid';
+import Item from '../components/Item';
+import Heading from '../components/Heading';
+import Button from '../components/Button';
+
+import { buttons, sizes } from '../assets/styles/constants';
+
+const { BRAVO, DELTA, GOLF, HOTEL } = sizes;
+const { PRIMARY } = buttons;
 
 export default () => (
   <Layout>
     <SEO title="404: Not found" />
-    {/* <Container>
-      <PageHeading>
-        <PageText
-          mainHeading="Page not found :-("
-          subHeading="You just hit a route that doesn't exist..."
-          paragraph="The sadness."
-        />
-      </PageHeading>
-    </Container> */}
+    <Container center>
+      <Graphic>404</Graphic>
+      <Grid withoutBottomSpacing>
+        <Item gridStart={[1, 2, 3, 1]} gridEnd={[6, 9, 11, 9]}>
+          <Heading as="h1" size={HOTEL}>
+            Uh oh!
+          </Heading>
+          <Heading sub as="h2" size={DELTA} noMargin>
+            The page you requested could not be found. But do not fear, there is
+            a way back.
+          </Heading>
+        </Item>
+        <Item gridStart={[1, 2, 3, 1]} gridEnd={[6, 4, 6, 6]} spacingTop={GOLF}>
+          <Button internalLink="/" buttonStyle={PRIMARY} buttonSize={BRAVO}>
+            Home
+          </Button>
+        </Item>
+      </Grid>
+    </Container>
   </Layout>
 );
