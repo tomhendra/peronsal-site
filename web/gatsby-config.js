f08da616b2ab673d8f6006bd6745e0809cc08f7a
Thomas Hendra <tom.hendra@outlook.com>
2020-04-25 21:41:45 +0200

    config: removed gatsby-plugin-emotion - doesn't work with emotion v11

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
         overlayDrafts: !isProd,
       },
     },
-    'gatsby-plugin-emotion',
+    // 'gatsby-plugin-emotion',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
