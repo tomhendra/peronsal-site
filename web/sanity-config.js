f3658a6ee9803ba1758d7b3b5a84e13513952757
Thomas Hendra <tom.hendra@outlook.com>
2020-06-14 22:44:00 +0200

    feat(config): development dataset for sanity - added new dataset - graphql delpoy to production before build step

--- web/sanity-config.js
+++ web/sanity-config.js
@@ -1,6 +1,6 @@
 // prettier-ignore
 module.exports = {
   projectId: process.env.SANITY_PROJECT_ID || '<#< sanity.projectId >#>',
-  dataset: process.env.SANITY_DATASET || '<#< sanity.dataset >#>',
   token: process.env.SANITY_AUTH_TOKEN || '<#< sanity.readToken >#>',
+  dataset: process.env.NODE_ENV === 'production' ? 'production' : 'development'
 };
