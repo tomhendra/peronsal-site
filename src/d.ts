8049556f475266ff8738b93bbe4bfe3b632eeccb
Tom Hendra <tom.hendra@outlook.com>
2021-11-29 12:39:48 +0100

    feat: astro init

---
+++ src/d.ts
@@ -0,0 +1,5 @@
+{
+}
+// To fix error: No inputs were found in config file
+// '/Users/tom/Developer/personal-site/tsconfig.json'. Specified 'include'
+// paths were '["**/*"]' and 'exclude' paths were '[]'.ts
