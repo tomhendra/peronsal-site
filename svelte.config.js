b4297dd50e129175ac5efc0456539ae6e50295a5
Tom Hendra <tom.hendra@outlook.com>
2022-02-04 17:04:24 +0100

    feat: css preprocessing prefixing

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,9 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 import {mdsvex} from 'mdsvex';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import autoprefixer from 'autoprefixer';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,12 +12,18 @@ const config = {
     adapter: adapter(),
   },
   extensions: ['.svelte', '.md'],
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for mo re information about preprocessors
+  // https://github.com/sveltejs/svelte-preprocess
   preprocess: [
-    preprocess(),
+    preprocess({
+      postcss: {
+        plugins: [autoprefixer],
+      },
+      /* Other sveltePreprocess options here, like SCSS */
+    }),
+    /* Other preprocessors here, like mdsvex */
     mdsvex({
       extensions: ['.md'],
+      rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
       layout: {
         blog: 'src/routes/blog/_post.svelte',
       },
