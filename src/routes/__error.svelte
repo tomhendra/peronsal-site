9748294a033ab026edee7f7ed398a1bd02beae62
Tom Hendra <tom.hendra@outlook.com>
2022-03-31 19:02:54 +0200

    feat: custom errror page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -11,9 +11,62 @@
   export let error: Error;
 </script>
 
-<h2>{status}</h2>
-<p>{error.message}</p>
+<div class="max-width-container">
+  <main>
+    <section class="header-section">
+      <span class="heading-prefix">{error.name} {status}</span>
+      <h1>{error.message}</h1>
+      <p class="subtitle">Oops! Something went wrong...</p>
+      <a href="/">Go back home</a>
+    </section>
+  </main>
+</div>
 
-<ul>
-  <li><a href="/">Home</a></li>
-</ul>
+<style lang="scss">
+  main {
+    padding-top: var(--space-24);
+    padding-bottom: var(--space-16);
+
+    @include tabletAndDown {
+      padding-top: var(--space-20);
+    }
+
+    @include mobileAndDown {
+      padding-top: var(--space-16);
+      padding-bottom: var(--space-12);
+    }
+  }
+
+  .header-section {
+    padding-bottom: var(--space-16);
+  }
+
+  .heading-prefix {
+    font-size: var(--font-size-md);
+    line-height: var(--line-height-md);
+    font-weight: var(--font-weight-semibold);
+    color: var(--color-primary-heading);
+  }
+
+  h1 {
+    margin-top: var(--space-3);
+  }
+
+  .subtitle {
+    font-size: var(--font-size-text-xl);
+    line-height: var(--line-height-text-xl);
+
+    @include mobileAndDown {
+      font-size: var(--font-size-text-lg);
+      line-height: var(--line-height-text-lg);
+    }
+  }
+
+  .max-width-container {
+    padding-bottom: var(--space-24);
+
+    @include mobileAndDown {
+      padding-bottom: var(--space-16);
+    }
+  }
+</style>
