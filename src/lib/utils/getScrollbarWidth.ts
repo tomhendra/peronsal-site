1f33b1be8d504edcdb82580e7c5d9ad6cfc532be
Tom Hendra <tom.hendra@outlook.com>
2022-04-04 16:48:45 +0200

    fix: layout shift caused by --full-width

--- src/lib/utils/getScrollbarWidth.ts
+++ src/lib/utils/getScrollbarWidth.ts
@@ -3,3 +3,24 @@ export function getScrollbarWidth() {
   const viewportWidthWithoutScrollbar = document.documentElement.clientWidth;
   return viewportWidth - viewportWidthWithoutScrollbar;
 }
+
+/* 
+  vw units target the viewport width excluding the scrollbar. to use vw units
+  without potential layout shift, we need to calculate the scrollbar width at 
+  the earliest opportunity; in this app it would be the main __layout.svelte
+  inside an onMount call. 
+
+  However, this causes a layout shift on the first render of a SvelteKit app as 
+  of 1.0.0-next.304 when used on .max-width-wrapper util style, so is unused.
+
+  (this issue does not exist in React + useEffect)
+*/
+
+export function setFullWidthCSSVariable() {
+  const scrollbarWidth = getScrollbarWidth();
+
+  document.documentElement.style.setProperty(
+    '--scrollbar-width',
+    scrollbarWidth + 'px',
+  );
+}
