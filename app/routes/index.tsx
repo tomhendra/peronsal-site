79d0aed3729c4a9b45bd75159cf8f27bbecfc45e
Tom Hendra <tom.hendra@outlook.com>
2022-04-20 20:55:02 +0200

    feat: hero section

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -22,117 +22,25 @@ function Index() {
       <MaxWidthContainer>
         <div className="hero-section">
           <div className="hero-title-section">
-            <IconWrapper>
-              <MapPin size={12} color="var(--color-primary)" />
-            </IconWrapper>
-            <span className="hero-location-text">Salamanca, Spain</span>
+            <div>
+              <IconWrapper>
+                <MapPin size={12} color="var(--color-primary-text)" />
+              </IconWrapper>
+              <span className="hero-location-text">Salamanca</span>
+            </div>
             <h1>Hello, I'm Tom</h1>
             <span className="hero-subtitle">
               I'm a Front End Developer from the UK based in Spain, who loves to
               build stuff for the web.
             </span>
-          </div>
-          <div className="button-group">
-            <Button variant="primary" size="md">
-              Button
-            </Button>
-            <Button variant="secondary" size="md">
-              Button
-            </Button>
-            <Button variant="secondary-gray" size="md">
-              Button
-            </Button>
-            <Button variant="tertiary" size="md">
-              Button
-            </Button>
-            <Button variant="tertiary-gray" size="md">
-              Button
-            </Button>
-            <Button variant="link" size="md">
-              Button
-            </Button>
-            <Button variant="link-gray" size="md">
-              Button
-            </Button>
-          </div>
-          <div className="button-group">
-            <Button variant="primary" size="md" disabled>
-              Button
-            </Button>
-            <Button variant="secondary" size="md" disabled>
-              Button
-            </Button>
-            <Button variant="secondary-gray" size="md" disabled>
-              Button
-            </Button>
-            <Button variant="tertiary" size="md" disabled>
-              Button
-            </Button>
-            <Button variant="tertiary-gray" size="md" disabled>
-              Button
-            </Button>
-            <Button variant="link" size="md" disabled>
-              Button
-            </Button>
-            <Button variant="link-gray" size="md" disabled>
-              Button
-            </Button>
-          </div>
-          <div className="button-group">
-            <Button variant="primary" size="md" mood="destructive">
-              Button
-            </Button>
-            <Button variant="secondary" size="md" mood="destructive">
-              Button
-            </Button>
-            <Button variant="secondary-gray" size="md" mood="destructive">
-              Button
-            </Button>
-            <Button variant="tertiary" size="md" mood="destructive">
-              Button
-            </Button>
-            <Button variant="tertiary-gray" size="md" mood="destructive">
-              Button
-            </Button>
-            <Button variant="link" size="md" mood="destructive">
-              Button
-            </Button>
-            <Button variant="link-gray" size="md" mood="destructive">
-              Button
-            </Button>
-          </div>
-          <div className="button-group">
-            <Button variant="primary" size="md" mood="destructive" disabled>
-              Button
-            </Button>
-            <Button variant="secondary" size="md" mood="destructive" disabled>
-              Button
-            </Button>
-            <Button
-              variant="secondary-gray"
-              size="md"
-              mood="destructive"
-              disabled
-            >
-              Button
-            </Button>
-            <Button variant="tertiary" size="md" mood="destructive" disabled>
-              Button
-            </Button>
-            <Button
-              variant="tertiary-gray"
-              size="md"
-              mood="destructive"
-              disabled
-            >
-              Button
-            </Button>
-            <Button variant="link" size="md" mood="destructive" disabled>
-              Button
-            </Button>
-            <Button variant="link-gray" size="md" mood="destructive" disabled>
-              Button
-            </Button>
+            <div className="btn-group">
+              <Button href="/projects" size="xl" variant="secondary-gray">
+                View Projects
+              </Button>
+              <Button to="#contact" size="xl" variant="primary">
+                Get in Touch
+              </Button>
+            </div>
           </div>
           <div className="hero-image-section">
             <img
@@ -142,15 +50,15 @@ function Index() {
             />
           </div>
         </div>
-        {/* <div className="about-section">
+        <div className="about-section">
           <h2 id="about">About</h2>
         </div>
         <div className="projects section">
           <h2>Projects</h2>
         </div>
-        <div className="contact-section">
+        <div className="contact-section" id="contact">
           <h2>Contact</h2>
-        </div> */}
+        </div>
       </MaxWidthContainer>
     </main>
   );
