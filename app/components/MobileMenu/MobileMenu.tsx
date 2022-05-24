dbf616bb9aa3dc991c1ddd8648315ed98a28c491
Tom Hendra <tom.hendra@outlook.com>
2022-05-24 22:56:30 +0200

    refactor: simplified mobile menu

--- app/components/MobileMenu/MobileMenu.tsx
+++ app/components/MobileMenu/MobileMenu.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import * as Dialog from "@radix-ui/react-dialog";
 import Link from "../Link";
-import MobileMenuButton from "./MobileMenuButton";
+import { Menu } from "react-feather";
+import VisuallyHidden from "~/components/VisuallyHidden";
 import type { LinksFunction } from "@remix-run/cloudflare";
 
 import { links as linkLinks } from "../Link";
-import { links as mobileMenuButtonLinks } from "./MobileMenuButton";
+import { links as visuallyHiddenLinks } from "~/components/VisuallyHidden";
 import styles from "./mobile-menu.css";
-import VisuallyHidden from "../VisuallyHidden";
 
 const links: LinksFunction = () => [
   ...linkLinks(),
-  ...mobileMenuButtonLinks(),
+  ...visuallyHiddenLinks(),
   { rel: "stylesheet", href: styles },
 ];
 
@@ -20,9 +20,12 @@ function MobileMenu() {
   const toggleMobileMenu = () => setIsOpen(!isOpen);
 
   return (
-    <Dialog.Root open={isOpen} onOpenChange={setIsOpen}>
+    <Dialog.Root open={isOpen} onOpenChange={toggleMobileMenu}>
       <Dialog.Trigger asChild>
-        <MobileMenuButton toggleMenu={toggleMobileMenu} />
+        <span className="mobile-menu-button">
+          <Menu />
+          <VisuallyHidden>Open and close the mobile menu</VisuallyHidden>
+        </span>
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="mobile-menu-overlay" />
@@ -34,31 +37,35 @@ function MobileMenu() {
             </Dialog.Description>
           </VisuallyHidden>
           <nav className="mobile-menu-nav">
-            <Dialog.Close asChild>
-              <Link variant="gray" size="lg" to="/">
-                Home
-              </Link>
-            </Dialog.Close>
-            <Dialog.Close asChild>
-              <Link variant="gray" size="lg" to="/#projects">
-                Projects
-              </Link>
-            </Dialog.Close>
-            <Dialog.Close asChild>
-              <Link variant="gray" size="lg" to="/#about">
-                About
-              </Link>
-            </Dialog.Close>
-            <Dialog.Close asChild>
-              <Link variant="gray" size="lg" to="/#contact">
-                Contact
-              </Link>
-            </Dialog.Close>
+            <Link variant="gray" size="lg" to="/" onClick={toggleMobileMenu}>
+              Home
+            </Link>
+            <Link
+              variant="gray"
+              size="lg"
+              to="/#projects"
+              onClick={toggleMobileMenu}
+            >
+              Projects
+            </Link>
+            <Link
+              variant="gray"
+              size="lg"
+              to="/#about"
+              onClick={toggleMobileMenu}
+            >
+              About
+            </Link>
+            <Link
+              variant="gray"
+              size="lg"
+              to="/#contact"
+              onClick={toggleMobileMenu}
+            >
+              Contact
+            </Link>
           </nav>
         </Dialog.Content>
-        <Dialog.Close asChild>
-          <MobileMenuButton toggleMenu={toggleMobileMenu} />
-        </Dialog.Close>
       </Dialog.Portal>
     </Dialog.Root>
   );
