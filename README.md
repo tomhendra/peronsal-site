5c548b446888fa5cc0b75d31ae940afa0fc76db6
Tom Hendra <tom.hendra@outlook.com>
2022-06-16 15:37:28 +0200

    chore: pkg bump

--- README.md
+++ README.md
@@ -14,31 +14,3 @@ commits with meaningful categories.
 • revert
 • style
 • test
-
-## Bugs
-
-1. on mobile menu toggle `style="pointer-events: none;"` is being
-   appended to the <body> tag but not removed on dialog close. maybe an error
-   with the button? also focus outline is visible on mobile and looks nasty.
-   check implementation & if no good, move to headless UI BUT ensure tab
-   component works correctly.
-   • https://github.com/radix-ui/design-system/blob/master/components/Sheet.tsx
-
-2. button background-color transition when leaving hover state is being
-   affected by theme toggle. `filter: brightness(110%)` is more performant.
-   consider refactoring.
-
-   • https://developer.mozilla.org/en-US/docs/Web/HTML/Element/meta/name/theme-color
-
-## Enhancements
-
-1. make the form sending experience amazing
-   • https://tobiasahlin.com/blog/meaningful-motion-w-action-driven-animation/
-
-2. make the the dark / light mode toggle interaction amazing.
-
-3. `--shadow-color` is currently being defined where it is used based on the
-   background using. figure out calc functions based on colour palettes.
-   • https://www.joshwcomeau.com/css/introducing-shadow-palette-generator/
-
-4. add theme color for Safari
--- app/components/MobileMenu/MobileMenu.tsx
+++ app/components/MobileMenu/MobileMenu.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import * as Dialog from "@radix-ui/react-dialog";
+import * as DialogPrimitive from "@radix-ui/react-Dialog";
 import Link from "../Link";
 import { Menu } from "react-feather";
 import VisuallyHidden from "~/components/VisuallyHidden";
@@ -15,59 +15,93 @@ const links: LinksFunction = () => [
   { rel: "stylesheet", href: styles },
 ];
 
+type SheetProps = React.ComponentProps<typeof DialogPrimitive.Root>;
+
+export function Sheet({ children, ...props }: SheetProps) {
+  return (
+    <DialogPrimitive.Root {...props}>
+      <DialogPrimitive.Overlay className="mobile-menu-overlay" />
+      {children}
+    </DialogPrimitive.Root>
+  );
+}
+
+type DialogContentPrimitiveProps = React.ComponentProps<
+  typeof DialogPrimitive.Content
+>;
+
+const SheetContent = React.forwardRef<
+  React.ElementRef<typeof DialogPrimitive.Content>,
+  DialogContentPrimitiveProps
+>(({ children, ...props }, forwardedRef) => (
+  <DialogPrimitive.Content
+    className="mobile-menu-content"
+    {...props}
+    ref={forwardedRef}
+  >
+    {children}
+  </DialogPrimitive.Content>
+));
+
+SheetContent.displayName = "SheetContent";
+
+export { SheetContent };
+export const SheetTrigger = DialogPrimitive.Trigger;
+export const SheetClose = DialogPrimitive.Close;
+export const SheetTitle = DialogPrimitive.Title;
+export const SheetDescription = DialogPrimitive.Description;
+
 function MobileMenu() {
   const [isOpen, setIsOpen] = React.useState<boolean>(false);
   const toggleMobileMenu = () => setIsOpen(!isOpen);
 
   return (
-    <Dialog.Root open={isOpen} onOpenChange={toggleMobileMenu}>
-      <Dialog.Trigger asChild>
+    <DialogPrimitive.Root open={isOpen} onOpenChange={toggleMobileMenu}>
+      <DialogPrimitive.Trigger asChild>
         <button className="mobile-menu-button" onClick={toggleMobileMenu}>
           <Menu />
           <VisuallyHidden>Open and close the mobile menu</VisuallyHidden>
         </button>
-      </Dialog.Trigger>
-      <Dialog.Portal>
-        <Dialog.Overlay className="mobile-menu-overlay" />
-        <Dialog.Content className="mobile-menu-content">
-          <VisuallyHidden>
-            <Dialog.Title>Menu</Dialog.Title>
-            <Dialog.Description>
-              Navigate to another part of the website.
-            </Dialog.Description>
-          </VisuallyHidden>
-          <nav className="mobile-menu-nav">
-            <Link variant="gray" size="lg" to="/" onClick={toggleMobileMenu}>
-              Home
-            </Link>
-            <Link
-              variant="gray"
-              size="lg"
-              to="/#projects"
-              onClick={toggleMobileMenu}
-            >
-              Projects
-            </Link>
-            <Link
-              variant="gray"
-              size="lg"
-              to="/#about"
-              onClick={toggleMobileMenu}
-            >
-              About
-            </Link>
-            <Link
-              variant="gray"
-              size="lg"
-              to="/#contact"
-              onClick={toggleMobileMenu}
-            >
-              Contact
-            </Link>
-          </nav>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
+      </DialogPrimitive.Trigger>
+      <DialogPrimitive.Overlay className="mobile-menu-overlay" />
+      <DialogPrimitive.Content className="mobile-menu-content">
+        <VisuallyHidden>
+          <DialogPrimitive.Title>Menu</DialogPrimitive.Title>
+          <DialogPrimitive.Description>
+            Navigate to another part of the website.
+          </DialogPrimitive.Description>
+        </VisuallyHidden>
+        <nav className="mobile-menu-nav">
+          <Link variant="gray" size="lg" to="/" onClick={toggleMobileMenu}>
+            Home
+          </Link>
+          <Link
+            variant="gray"
+            size="lg"
+            to="/#projects"
+            onClick={toggleMobileMenu}
+          >
+            Projects
+          </Link>
+          <Link
+            variant="gray"
+            size="lg"
+            to="/#about"
+            onClick={toggleMobileMenu}
+          >
+            About
+          </Link>
+          <Link
+            variant="gray"
+            size="lg"
+            to="/#contact"
+            onClick={toggleMobileMenu}
+          >
+            Contact
+          </Link>
+        </nav>
+      </DialogPrimitive.Content>
+    </DialogPrimitive.Root>
   );
 }
 
