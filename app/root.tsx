e15f1f2b3134c2f7d18ee0d79e717efb155a4021
Tom Hendra <tom.hendra@outlook.com>
2022-06-29 20:14:38 +0200

    style: dark mode optimisations

--- app/root.tsx
+++ app/root.tsx
@@ -53,11 +53,22 @@ const meta: MetaFunction = () => ({
   title: 'Tom Hendra • Personal Site',
   description: 'Personal site for Tom Hendra, Web Developer.',
   viewport: 'width=device-width,initial-scale=1',
+  // css vars do not work here for content :(
+  themeColorDark: {
+    name: 'theme-color',
+    content: 'hsl(220deg 55% 8% / 100%)',
+    media: '(prefers-color-scheme: dark)',
+  },
+  themeColorLight: {
+    name: 'theme-color',
+    content: 'hsl(0deg 0% 100% / 100%)',
+    media: '(prefers-color-scheme: light)',
+  },
 });
 
 const links: LinksFunction = () => [
   /* light and dark versions are needed for Chrome, possibly due to SSR ?
-    order of <link rel="icon"> statements matters */
+    order of <link rel="icon"> statements matters. */
   {
     rel: 'icon',
     href: '/images/favicon-light-mode.svg',
@@ -71,14 +82,17 @@ const links: LinksFunction = () => [
     media: '(prefers-color-scheme: dark)',
   },
   /* favicon.svg has the adaptive color styles inline within the markup,
-    which works in Firefox but not in Chrome. */
+    which works in Firefox but not in Chrome. this needs to come after the 
+    light / dark variants to as to be selected by Firefox. */
   {
     rel: 'icon',
     href: '/images/favicon.svg',
     type: 'image/svg+xml',
   },
   /* favicon.ico for browsers that don't support svg yet (Safari)
-    https://caniuse.com/link-icon-svg */
+    https://caniuse.com/link-icon-svg. Many guides advise for this to be 
+    specified first, and that sizes="any" prevents Chrome from selecting the ico, 
+    however I have found neither to prevent Chrome from selecting favicon.ico */
   {
     rel: 'icon',
     href: '/images/favicon.ico',
