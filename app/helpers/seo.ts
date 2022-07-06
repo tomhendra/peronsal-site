49830418f172cc069d4027be7d8cf721194076b0
Tom Hendra <tom.hendra@outlook.com>
2022-07-06 14:55:16 +0200

    fix: og image

--- app/helpers/seo.ts
+++ app/helpers/seo.ts
@@ -12,12 +12,12 @@ export const {getSeo, getSeoMeta, getSeoLinks} = initSeo({
     url: 'https://tomhendra.com',
     images: [
       {
-        url: 'https://res.cloudinary.com/tomhendra/image/upload/v1657101258/personal-site/open-graph-twitter-card-summary-large.png',
+        url: 'https://res.cloudinary.com/tomhendra/image/upload/v1657112058/personal-site/open-graph-card.png',
         secureUrl:
-          'https://res.cloudinary.com/tomhendra/image/upload/v1657101258/personal-site/open-graph-twitter-card-summary-large.png',
+          'https://res.cloudinary.com/tomhendra/image/upload/v1657112058/personal-site/open-graph-card.png',
         alt: 'Stylised cute robot logo with an introduction: Tom Hendra, Frontend developer, @tomhendra and tomhendra.com',
-        height: 640,
-        width: 1280,
+        height: 1280,
+        width: 2560,
         type: 'image/png',
       },
     ],
@@ -33,3 +33,5 @@ export const {getSeo, getSeoMeta, getSeoLinks} = initSeo({
     description: 'Frontend developer building for the web.',
   },
 });
+
+// 2049 x 1068
