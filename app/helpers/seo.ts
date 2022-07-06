ea9e2daf1b5d1b855d76ea562fd24cf7b3402a38
Tom Hendra <tom.hendra@outlook.com>
2022-07-06 13:22:23 +0200

    fix: og image

--- app/helpers/seo.ts
+++ app/helpers/seo.ts
@@ -3,34 +3,33 @@ import {initSeo} from 'remix-seo';
 export const {getSeo, getSeoMeta, getSeoLinks} = initSeo({
   title: 'Hello',
   titleTemplate: '%s | Tom Hendra',
-  description: 'Front end developer.',
+  description: 'Frontend developer building for the web.',
   openGraph: {
     siteName: "Tom Hendra's personal website",
     title: 'Tom Hendra',
-    description: 'Front end developer.',
+    description: 'Frontend developer building for the web.',
     type: 'website',
     url: 'https://tomhendra.com',
     images: [
       {
-        url: 'https://res.cloudinary.com/tomhendra/image/upload/v1657050188/personal-site/open-graph-logo.png',
+        url: 'https://res.cloudinary.com/tomhendra/image/upload/v1657101258/personal-site/open-graph-twitter-card-summary-large.png',
         secureUrl:
-          'https://res.cloudinary.com/tomhendra/image/upload/v1657050188/personal-site/open-graph-logo.png',
-        alt: 'Tom Hendra logo - a stylised cute robot',
-        height: 1500,
-        width: 1500,
+          'https://res.cloudinary.com/tomhendra/image/upload/v1657101258/personal-site/open-graph-twitter-card-summary-large.png',
+        alt: 'Stylised cute robot logo with an introduction: Tom Hendra, Frontend developer, @tomhendra and tomhendra.com',
+        height: 640,
+        width: 1280,
         type: 'image/png',
       },
     ],
   },
   twitter: {
     card: 'summary_large_image',
-    creator: '@tomhendra',
     image: {
-      url: 'https://res.cloudinary.com/tomhendra/image/upload/v1657051757/personal-site/open-graph-twitter-summary-large.png',
-      alt: 'Tom Hendra summary card - a stylised cute robot logo with introduction: Tom Hendra, Frontend Developer, @tomhendra tomhendra.com',
+      url: 'https://res.cloudinary.com/tomhendra/image/upload/v1657101258/personal-site/open-graph-twitter-card-summary-large.png',
+      alt: 'Tom Hendra twitter summary card featuring a stylised cute robot logo with an introduction: Tom Hendra, Frontend developer, @tomhendra, tomhendra.com',
     },
     site: '@tomhendra',
     title: 'Tom Hendra',
-    description: 'Front end developer.',
+    description: 'Frontend developer building for the web.',
   },
 });
