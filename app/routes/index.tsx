40e1d49aff751addf779f89c615d1165d4311f73
Tom Hendra <tom.hendra@outlook.com>
2022-07-11 21:06:08 +0200

    chore: pkg bump

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -117,8 +117,8 @@ function Index() {
               </div>
               <h1>Hello, I'm Tom</h1>
               <h2 className="section-subheading">
-                I'm a developer from the UK who loves to build quality user
-                interfaces for the web.
+                I'm a frontend developer from the UK who loves to build quality
+                user interfaces for the web.
               </h2>
               <div className="btn-group">
                 <Button
