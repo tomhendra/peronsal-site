c2a3aa1c87936ca622efb5aaec4101783f2ad08c
Tom Hendra <tom.hendra@outlook.com>
2022-12-14 18:28:12 +0100

    feat: seo changes

--- app/helpers/seo.ts
+++ app/helpers/seo.ts
@@ -2,11 +2,11 @@ import {initSeo} from 'remix-seo';
 
 export const {getSeo, getSeoMeta, getSeoLinks} = initSeo({
   title: 'Tom Hendra',
-  description: 'Front-end developer building for the web.',
+  description: 'Software developer building for the web.',
   openGraph: {
     siteName: "Tom Hendra's personal website",
     title: 'Tom Hendra',
-    description: 'Front-end developer building for the web.',
+    description: 'Software developer building for the web.',
     type: 'website',
     url: 'https://tomhendra.com',
     images: [
@@ -14,7 +14,7 @@ export const {getSeo, getSeoMeta, getSeoLinks} = initSeo({
         url: 'https://res.cloudinary.com/tomhendra/image/upload/v1657112058/personal-site/open-graph-card.png',
         secureUrl:
           'https://res.cloudinary.com/tomhendra/image/upload/v1657112058/personal-site/open-graph-card.png',
-        alt: 'Stylised cute robot logo with an introduction: Tom Hendra, Frontend developer, @tomhendra and tomhendra.com',
+        alt: 'Stylised cute robot logo with an introduction: Tom Hendra, Software developer, @tomhendra and tomhendra.com',
         height: 1280,
         width: 2560,
         type: 'image/png',
@@ -25,6 +25,6 @@ export const {getSeo, getSeoMeta, getSeoLinks} = initSeo({
     card: 'summary_large_image',
     site: '@tomhendra',
     title: 'Tom Hendra',
-    description: 'Frontend developer building for the web.',
+    description: 'Software developer building for the web.',
   },
 });
