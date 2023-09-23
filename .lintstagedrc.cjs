4ac1ce8fd41d9cd475fd899cfdbfb226225941bf
Tom Hendra <tom.hendra@outlook.com>
2023-09-23 13:24:06 +0200

    style: custom fonts

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
   // lint & format
   '**/*.(ts|tsx|js|astro|mdx)': (filenames) => [
     `eslint ${filenames.join(' ')}`,
-    `prettier --write ${filenames.join(' ')}`,
+    `prettier --write ${filenames.join(' ')} --plugin=prettier-plugin-astro`,
   ],
   // format only
   '**/*.+(json|md)': (filenames) => `prettier --write ${filenames.join(' ')}`,
