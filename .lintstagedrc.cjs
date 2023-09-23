ffe695a1ead3c0ff84259a9b01503d7f08a5828f
Tom Hendra <tom.hendra@outlook.com>
2023-09-23 12:26:13 +0200

    chore: more editor setup

---
+++ .lintstagedrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  // type check TypeScript files
+  '**/*.+(ts|tsx)': () => 'tsc -p tsconfig.json --noEmit',
+  // type check Astro files
+  '**/*.+(astro)': () => 'astro check',
+  // lint & format
+  '**/*.(ts|tsx|js|astro|mdx)': (filenames) => [
+    `eslint ${filenames.join(' ')}`,
+    `prettier --write ${filenames.join(' ')}`,
+  ],
+  // format only
+  '**/*.+(json|md)': (filenames) => `prettier --write ${filenames.join(' ')}`,
+}
