95cf546e517e820948626dcaf40afbbde0dc6a52
Tom Hendra <tom.hendra@outlook.com>
2023-10-08 11:26:15 +0200

    feat: fresh

--- src/constants.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Tom Hendra'
-export const SITE_DESCRIPTION = 'React Native Dev based in Spain'
--- src/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
-declare global {
-  var ThemeProvider: {
-    updatePickers(theme?: string): void
-  }
-}
-
-export {}
