b3374a61b0b9fa86075d5f480117e43d9ba44e39
Tom Hendra <tom.hendra@outlook.com>
2023-10-13 15:16:10 +0200

    chore: refresh fresh

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -4,9 +4,10 @@ export default function App({ Component }: AppProps) {
   return (
     <html>
       <head>
-        <meta charSet="utf-8" />
+        <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>tomhendra.dev</title>
+        <title>fresh-project</title>
+        <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
         <Component />
