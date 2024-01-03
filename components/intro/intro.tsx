51773e50f3b09e2974d4e20456044a6e23c8f117
Tom Hendra <tom.hendra@outlook.com>
2024-01-03 12:03:55 +0100

    feat: wembley link

--- components/intro/intro.tsx
+++ components/intro/intro.tsx
@@ -3,19 +3,17 @@ import { Highlight, Link } from "../index.ts";
 export function Intro() {
   return (
     <p class="intro">
-      Builder of <Highlight>React Native</Highlight> apps at Wembley Studios in
-      {" "}
-      <Highlight>Salamanca</Highlight>, Spain. Norwich,{" "}
+      Builder of <Highlight>React Native</Highlight> apps at{" "}
+      <Highlight>
+        <Link href="https://wembleystudios.com/" target="_blank">
+          Wembley Studios
+        </Link>
+      </Highlight>{" "}
+      in <Highlight>Salamanca</Highlight>, Spain. Norwich,{" "}
       <Highlight>England native</Highlight>. UX and animation lover. Immersed in
       local food culture or the gaming world when not coding. Hola @
       <Highlight>
-        <Link
-          href="https://x.com/tomhendra"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          tomhendra
-        </Link>
+        <Link href="https://x.com/tomhendra" target="_blank">tomhendra</Link>
         .
       </Highlight>
     </p>
