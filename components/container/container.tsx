b1cdc606715a3190980f31df5c6b12c0034a6fe1
Tom Hendra <tom.hendra@outlook.com>
2024-04-07 19:21:05 +0200

    chore: clean branch

--- components/container/container.tsx
+++
@@ -1,7 +0,0 @@
-import { JSX } from "preact/jsx-runtime";
-
-export function Container(
-  { children, ...rest }: JSX.HTMLAttributes<HTMLDivElement>,
-) {
-  return <div class="container" {...rest}>{children}</div>;
-}
