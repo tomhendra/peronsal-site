0158d2a3fb2d98612b2720a182229904cb1d30fb
Tom Hendra <tom.hendra@outlook.com>
2024-09-01 16:33:18 +0200

    fix: lint error

--- server.ts
+++ server.ts
@@ -24,7 +24,7 @@ fastify.setNotFoundHandler((request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    const port = parseInt(process.env.PORT || '3000', 10)
+    const port = Number.parseInt(process.env.PORT || '3000', 10)
     const host = process.env.HOST || '0.0.0.0'
 
     await fastify.listen({ port, host })
